26ec7fa1653aa5810173e5dd24083a75b245f621
Vilius Tamašauskas <viliustam36@gmail.com>
2024-11-10 00:46:19 +0200

    home updated

--- backend/db.js
+++ backend/db.js
@@ -1,25 +1,25 @@
 const mysql = require('mysql2');
 
-const dbConfig = {
-  host: 'mysql',
-  user: 'stud',
-  password: 'stud',
-  database: 'database',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-};
-
-
 // const dbConfig = {
-//   host: 'localhost',
-//   user: 'root',
-//   password: '',
-//   database: 'projektas',
+//   host: 'mysql',
+//   user: 'stud',
+//   password: 'stud',
+//   database: 'database',
 //   waitForConnections: true,
 //   connectionLimit: 10,
 //   queueLimit: 0
 // };
+
+
+const dbConfig = {
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'projektas',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+};
 
 const pool = mysql.createPool(dbConfig);
 
--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -4,6 +4,26 @@ import BookTable from '../components/table/bookTable';
 const Home = () => {
   return (
     <div>
+      <h1>IFF-2/1 Vilius Tamašauskas</h1>
+      <div className="library-system-description">
+        <h2>Bibliotekos sistema</h2>
+        <p>Administratorius registruoja bibliotekininkus.</p>
+        <p>Bibliotekininkas redaguoja knygų sąrašus ir egz. skaičių.</p>
+        <p>Neprisiregistravęs vartotojas mato sąrašus (visų ir nepaimtų).</p>
+        <p>
+          Prisiregistravęs vartotojas išsirenka norimus iš nepaimtų ir laikom
+          kad pasiima knygas po 1 egz.
+        </p>
+        <p>Bibliotekininkas mato kas ką paėmęs, atžymi grąžinimą.</p>
+        <p>--- papildomai:</p>
+        <p>
+          Įvedam įvykių laikus. Knyga išduodama fiksuotam laikui. Jam artėjant į
+          pabaigą, siunčiama žinutė
+        </p>
+        <p>
+          Vartotojui, pas kurį yra laiku negrąžintų knygų, naujos neišduodamos.
+        </p>
+      </div>
       <BookTable />
     </div>
   );
