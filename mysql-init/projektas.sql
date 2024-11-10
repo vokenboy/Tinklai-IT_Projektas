ac4d10abe37d7bfdcbc4669dede7fa3dbc73b7df
Vilius Tamašauskas <viliustam36@gmail.com>
2024-11-11 01:20:24 +0200

    sql import

--- mysql-init/projektas.sql
+++ mysql-init/projektas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 10, 2024 at 08:52 PM
+-- Generation Time: Nov 11, 2024 at 12:19 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
