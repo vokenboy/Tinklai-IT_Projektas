26788b9c307090896105299e7823e543128f3c8d
Vilius Tamašauskas <viliustam36@gmail.com>
2024-12-04 14:20:02 +0200

    Update projektas.sql

--- mysql-init/projektas.sql
+++ mysql-init/projektas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 04, 2024 at 01:15 PM
+-- Generation Time: Dec 04, 2024 at 01:19 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
